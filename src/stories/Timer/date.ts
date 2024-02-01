0c2abdd4c989b0176c89ff200c8cca733eea7607
Jchen <jcdigital316@gmail.com>
2024-02-01 00:51:53 -0800

    remove unsed file

--- src/stories/Timer/date.ts
+++
@@ -1,8 +0,0 @@
-export const getShortFormattedTimeString = (timeMs: number) => {
-  const time = Math.floor(timeMs / 1000);
-  const minutes = Math.floor(time / 60);
-  const seconds = ("0" + (time % 60)).slice(-2);
-
-  if (time <= 0) return `0:00`;
-  return `${minutes}:${seconds}`;
-};
